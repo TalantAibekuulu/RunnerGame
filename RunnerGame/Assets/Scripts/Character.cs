3e76d77694b30ee8fba667a9a3765474cc5f8177
Aibek uulu Talant <aibekuulutalant451@gmail.com>
2024-04-21 22:52:20 +0600

    4lesson

--- RunnerGame/Assets/Scripts/Character.cs
+++ RunnerGame/Assets/Scripts/Character.cs
@@ -8,9 +8,16 @@ public class Character : MonoBehaviour
     private bool isJamping;
     private void Update()
     {
-        Run(); 
+        Run();
+        bool isSpaceClicked = Input.GetKeyUp(KeyCode.Space);
+
+        if (isSpaceClicked == true)
+        {
+            Boost(2);
+        }
+
     }
-    
+
     public void Boost(float miltiplier)
     {
         speed = speed * miltiplier;
@@ -21,13 +28,31 @@ public class Character : MonoBehaviour
     }
     private void Run()
     {
+        transform.position = GetNextPosition(speed);
+    }
+    private Vector3 GetNextPosition(float steplenth)
+    {
         Vector3 newPosition = new Vector3();
         newPosition.x = transform.position.x;
         newPosition.y = transform.position.y;
-        newPosition.z = transform.position.z + speed;
+        newPosition.z = transform.position.z + steplenth;
 
         transform.position = newPosition;
+        return newPosition;
+    }
+    private int GetSum(int firstNumber, int secondNumber)
+    {
+        int sum = 0;
+        sum += firstNumber + secondNumber;
+        return sum;
+    }
 
+
+    private float ЖарымынБер(int number)
+    {
+        float result = 0;
+        result += number / 2;
+        return result;
     }
 
 }
