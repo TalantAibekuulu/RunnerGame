d23b5abb4e67066596d8ff8442fd2ab48a1e1e1e
Aibek uulu Talant <aibekuulutalant451@gmail.com>
2024-04-27 07:40:18 +0600

    LevelBlock1,2,3.

--- RunnerGame/Assets/Scripts/ColiderTest.cs
+++ RunnerGame/Assets/Scripts/ColiderTest.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ColiderTest : MonoBehaviour
@@ -8,7 +6,7 @@ public class ColiderTest : MonoBehaviour
     {
         Debug.Log("Entered:"+other.gameObject.name);
     }
-    private void OnTriggerExited(Collider other)
+    private void OnTriggerExit(Collider other)
     {
         Debug.Log("Exited:" + other.gameObject.name);
     }
